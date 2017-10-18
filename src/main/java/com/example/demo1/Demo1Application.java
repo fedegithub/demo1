8b1af589d0bd782924aabe50742e1e549d469c7d
Fedro <frossini@hotmail.it>
2017-10-18 12:23:10 +0200

    First commit

---
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,51 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String [ ] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+}
+
+@Entity
+class Cat {
+
+@Id
+@GeneratedValue
+private Long id;
+
+private String name ;
+
+Cat() {
+}
+
+public Cat(String name) {
+	this.name=name;
+}
+
+@Override
+public String toString() {
+	return "Cat{"+"id="+id+", name='"+name+'\''+'}';
+}
+
+public Long getId() {
+	return id;
+}
+
+public String getName() {
+	return name;
+}
+}
+
+@RepositoryRestResource
+interface CatRepository extends JpaRepository < Cat , Long > {
+}
